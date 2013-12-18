3c45dcb8f0fe0f506c3343a11084aebee5cb4670
eric@triplelift.com <eric@triplelift.com@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-12-18 22:17:51 +0000

    1.5.0 tag
    git-svn-id: https://plugins.svn.wordpress.org/triplelift-native-advertising/tags/1.5.0@825083 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- html/manage_single_tag.php
+++ html/manage_single_tag.php
@@ -16,20 +16,38 @@ if (isset($this->error_message) && $this->error_message) {
 	<form name="modify_placement" method="post" action="<?php print TRIPLELIFT_NP_BASE_URL;?>" class="fancy-form">
 		<input type="hidden" name="<?php print $this->action_field;?>" value="modify_single_tag">		
 		<input type="hidden" name="triplelift_np_admin_original_script" value="<?php print $this->tag_settings['script']; ?>">
-        <p class="textarea">
-		    <textarea rows="2" cols="75" id="triplelift_np_admin_script_html" name="triplelift_np_admin_script_html"><?php print str_replace('/textarea', '', stripslashes($this->tag_settings['script'])); ?></textarea>
-        </p>
-		<p><?php print $this->tag_manager->render_offset($this->tag_settings, 'triplelift_np_admin_offset');?></p>
-		<p><?php print $this->tag_manager->render_interval($this->tag_settings, 'triplelift_np_admin_interval');?></p>
-		<p><?php print $this->tag_manager->render_active($this->tag_settings, 'triplelift_np_admin_active');?></p>
+		<input type="hidden" name="triplelift_np_admin_tl_last_update" value="<?php print (isset($this->tag_settings['tl_last_update']) ? $this->tag_settings['tl_last_update'] : 0); ?>">
+        <div class="stuffbox" id="namediv">
+           <h3><label for="name" style="font-size:14px">Script Tag</label></h3>
+           <div class="inside">
+ 
+                <p class="textarea">
+		            <textarea rows="2" cols="75" id="triplelift_np_admin_script_html" name="triplelift_np_admin_script_html"><?php print str_replace('/textarea', '', stripslashes($this->tag_settings['script'])); ?></textarea>
+                </p>
+           </div>
+        </div>
+        <div class="stuffbox" id="namediv">
+           <h3><label for="name" style="font-size:14px">General Settings</label></h3>
+           <div class="inside">
+        		<p><?php print $this->tag_manager->render_offset($this->tag_settings, 'triplelift_np_admin_offset');?></p>
+        		<p><?php print $this->tag_manager->render_interval($this->tag_settings, 'triplelift_np_admin_interval');?></p>
+        		<p><?php print $this->tag_manager->render_active($this->tag_settings, 'triplelift_np_admin_active');?></p>
+            </div>
+        </div>
         <p id="triplelift_np_admin_show_advanced_options"><a href="#">Show advanced options</a></p>
-        <div id="triplelift_np_admin_advanced_options" style="display: none;">
-		<p><?php print $this->tag_manager->render_wp_settings($this->tag_settings, 'triplelift_np_admin_wp_include_settings[]', 'include');?></p>
-		<p><?php print $this->tag_manager->render_wp_settings($this->tag_settings, 'triplelift_np_admin_wp_exclude_settings[]', 'exclude');?></p>
-		<p><?php print $this->tag_manager->render_admin_only_preview($this->tag_settings, 'triplelift_np_admin_admin_preview');?></p>
-		<p><?php print $this->tag_manager->render_path_settings($this->tag_settings, 'triplelift_np_admin_include_paths', 'include');?></p>
-		<p><?php print $this->tag_manager->render_path_settings($this->tag_settings, 'triplelift_np_admin_exclude_paths', 'exclude');?></p>
-		<p><?php print $this->tag_manager->render_tag_hooks($this->tag_settings, 'triplelift_np_admin_hook');?></p>
+        <div id="triplelift_np_admin_advanced_options"  style="display: none;">
+            <div class="stuffbox" id="namediv">
+               <h3><label for="name" style="font-size:14px">Advanced Settings</label></h3>
+                <div class="inside">
+                    <p><?php print $this->tag_manager->render_append_prepend_settings($this->tag_settings, 'triplelift_np_admin_append_prepend', 'include');?></p>
+                    <p><?php print $this->tag_manager->render_wp_settings($this->tag_settings, 'triplelift_np_admin_wp_include_settings[]', 'include');?></p>
+                    <p><?php print $this->tag_manager->render_wp_settings($this->tag_settings, 'triplelift_np_admin_wp_exclude_settings[]', 'exclude');?></p>
+                    <p class="width:4px"><?php print $this->tag_manager->render_admin_only_preview($this->tag_settings, 'triplelift_np_admin_admin_preview');?></p>
+                    <p><?php print $this->tag_manager->render_path_settings($this->tag_settings, 'triplelift_np_admin_include_paths', 'include');?></p>
+                    <p><?php print $this->tag_manager->render_path_settings($this->tag_settings, 'triplelift_np_admin_exclude_paths', 'exclude');?></p>
+                    <p><?php print $this->tag_manager->render_tag_hooks($this->tag_settings, 'triplelift_np_admin_hook');?></p>
+                </div>
+           </div>
         </div>
 		<p><input type="submit" name="Save" class="button-primary" value="<?php esc_attr_e('Save') ?>" />&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<a class="triplelift_np_admin_confirmation" href="<?php print TRIPLELIFT_NP_BASE_URL.'&tab=modify_tag&'.$this->action_field.'=delete_tag&tag='.urlencode($this->tag_settings['script']);?>">Delete</a></p>
 
--- js/add_tag.php
+++ js/add_tag.php
@@ -5,11 +5,14 @@ include_once(dirname(__FILE__).'/../js/theme_placement_add.php');
 ?>
 <script>
 function triplelift_np_admin_add_tag(target_div) {
+
+    triplelift_np_admin_html_placement_add(target_div);
+    /*
     jQuery("#"+target_div).html(
         '<ul class="card-list">'+
-            '<li><a href="#" id="triplelift_np_admin_sent_html"><strong>Tag</strong><br>&nbsp;<br><span class="card-list-text">TripleLift sent me a tag</span></a></li>'+
-            '<li><a href="#" id="triplelift_np_admin_theme_placement"><strong>Theme</strong><br>&nbsp;<br><span class="card-list-text">Plugin will make appropriate placement for my theme</span></a></li>'+
-            '<li><a href="#" id="triplelift_np_admin_existing_placement"><strong>Advanced</strong><br>&nbsp;<br><span class="card-list-text">Use existing placement</span></a></li></ul>');
+            '<li><a href="#" id="triplelift_np_admin_sent_html"><strong>Tag</strong><br>&nbsp;<br><span class="card-list-text">TripleLift sent me a tag <br>(Recommended)</span></a></li>'+
+            '<li><a href="#" id="triplelift_np_admin_existing_placement"><strong>Advanced</strong><br>&nbsp;<br><span class="card-list-text">Use existing placement</span></a></li>'+
+            '<li><a href="#" id="triplelift_np_admin_theme_placement"><strong>Theme</strong><br>&nbsp;<br><span class="card-list-text">Plugin makes placement from theme<br>(Experimental)</span></a></li></ul>');
     
 	jQuery("#triplelift_np_admin_existing_placement").click(function() {
     	triplelift_np_admin_load_publishers(target_div, false);
@@ -22,6 +25,7 @@ function triplelift_np_admin_add_tag(target_div) {
 	jQuery("#triplelift_np_admin_theme_placement").click(function() {
     	triplelift_np_admin_theme_placement_add(target_div);
 	});
+    */
 }
 
 </script>
